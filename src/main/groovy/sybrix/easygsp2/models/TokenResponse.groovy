e3c1f88daa27dabc80039fbbfd5e1843c8d49445
sybrix <dsmith@sybrix.com>
2025-02-14 19:22:43 -0800

    added profileId

--- src/main/groovy/sybrix/easygsp2/models/TokenResponse.groovy
+++ src/main/groovy/sybrix/easygsp2/models/TokenResponse.groovy
@@ -1,10 +1,15 @@
 package sybrix.easygsp2.models
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+
 class TokenResponse {
     String idToken
     String tokenType
     String username
     Long expiryDays
+    Long profileAdId
+    @JsonIgnore
+    Long profileId
     Map data = [:]
 }
 
